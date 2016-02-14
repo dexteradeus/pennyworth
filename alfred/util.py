c848e0701ea65a78382d8fcb59f1c1a1aa3f3998
Chris Hand <dexteradeus@users.noreply.github.com>
2016-02-14 14:01:45 -0500

    Add disconnect decorator
    - Handle closing the socket after each use regardless of exceptions that
      may happen

--- alfred/util.py
+++ alfred/util.py
@@ -1,5 +1,18 @@
 
+from functools import wraps
 from random import randint
 
 def get_random_id():
     return randint(0, 2**16)
+
+def disconnect(fn):
+    @wraps(fn)
+    def wrapper(self, *args, **kwargs):
+        try:
+            return fn(self, *args, **kwargs)
+        except:
+            raise
+        finally:
+            if self._connected:
+                self._disconnect()
+    return wrapper
